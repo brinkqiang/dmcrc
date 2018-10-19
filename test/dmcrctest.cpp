c696e392e0229e5efcc927b219951d4466c44662
brink.qiang <brink.qiang@gmail.com>
2018-10-19 21:05:30 +0800

    add

--- test/dmcrctest.cpp
+++ test/dmcrctest.cpp
@@ -1,9 +1,8 @@
 
 #include "dmcrc.h"
-#include "dmformat.h"
 #include <iostream>
 
 int main( int argc, char* argv[] ) {
-    std::cout << fmt::format("{0} {1}", "hello world! ", GetCRC32("hello world!"));
+    std::cout << "hello world! " << GetCRC32("hello world!") << std::endl;
     return 0;
 }
