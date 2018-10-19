7921fc06f4a4d99c41ec29b16c437142778de6e8
brink.qiang <brink.qiang@gmail.com>
2018-10-19 22:31:39 +0800

    add

--- test/dmcrctest.cpp
+++ test/dmcrctest.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 
 int main( int argc, char* argv[] ) {
+
+    static uint32_t gTable[256] = { 0 };
+    int nLen = sizeof(*gTable);
+
     std::cout << "hello world! " << GetCRC32("hello world!") << std::endl;
     return 0;
 }
